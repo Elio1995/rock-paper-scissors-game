83e4b70ae000716706d18999a6f4b58def085ee3
Elio1995 <mehmetielio95@gmail.com>
2022-08-09 11:18:36 +0200

    Fixing the positioning of stuff

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,20 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <img src="../images/bg-triangle.svg"></img>
+      <div
+        style={{
+          textAlign: "start",
+        }}
+      >
+        <ul>
+          <li>ROCK</li>
+          <li>PAPER</li>
+          <li>SCISSORS</li>
+        </ul>
+      </div>
+      <div style={{ marginTop: "250px" }}>
+        <img src="../images/bg-triangle.svg"></img>
+      </div>
     </div>
   );
 }
